e11d54c9f4f670e78dfca7ce9c27f5a797744d6a
sbwdlihao <sbwdlihao@gmail.com>
2017-09-28 16:38:36 +0800

    add glog

--- main.cpp
+++ main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <glog/logging.h>
 
 int main() {
+    google::InitGoogleLogging("main");
+    FLAGS_logtostderr = true;
     std::cout << "Hello, World!" << std::endl;
+    LOG(INFO) << "Found " << 5 << " cookies";
     return 0;
 }
--- sub_module0/sub0.cpp
+++ sub_module0/sub0.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <glog/logging.h>
 
 int main() {
+    google::InitGoogleLogging("sub0");
+    FLAGS_logtostderr = true;
     std::cout << "This is sub module0!" << std::endl;
+    LOG(INFO) << "Sub0 Found " << 5 << " cookies";
     return 0;
 }
